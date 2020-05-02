0547db1a6ba9251fab85c4c959ea8c8d1d4405a3
samuel martinez <samuel.martinez.morales@gmail.com>
2020-05-02 14:07:25 -0500

    condicional para Mostrar un componente un otro

--- src/App.js
+++ src/App.js
@@ -7,24 +7,38 @@ function App() {
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
 
+  //State condicional para mostrar u ocultar pregunta
+  const [mostrarpregunta, actualizarPregunta] = useState(true)
+
 
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta 
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
-          <div className="row">
-            <div className="one-half column">
-              <Formulario/>
-            </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+          {mostrarpregunta 
+            ?
+            ( 
+              <Pregunta 
+                guardarPresupuesto={guardarPresupuesto}
+                guardarRestante={guardarRestante}
+                actualizarPregunta={actualizarPregunta}
+              />
+            )
+            : 
+            (
+              <div className="row">
+                <div className="one-half column">
+                  <Formulario/>
+                </div>
+                <div className="one-half column">
+                  2
+                </div>
+              </div>
+            )
+          }
+          
+          
 
         </div>  
       </header>
